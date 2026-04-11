6731ed1e95a9f56ccd450f751efc71668fcfac8f
Filip Hultén <filip.hulten@si.se>
2026-04-11 20:03:44 +0200

    Completed

--- index.js
+++ index.js
@@ -1,19 +1,93 @@
-const btn = document.getElementById('button');
+let runningTotal = 0;
+let buffer = "0";
+let previousOperator;
 
-if (btn) {
-	btn.addEventListener('click', () => {
-		alert(`Example alert Button clicked`);
-	});
-} else {
-	console.warn('Button with id "button" not found.');
+const screen = document.querySelector(".screen");
+
+function buttonClick(value) {
+  if (isNaN(value)) {
+    handleSymbol(value);
+  } else {
+    handleNumber(value);
+  }
+  screen.innerText = buffer;
 }
 
-const btn2 = document.getElementById('button2');
+function handleNumber(numberString) {
+  if (buffer === "0") {
+    buffer = numberString;
+  } else {
+    buffer += numberString;
+  }
+}
 
-if (btn2) {
-    btn2.addEventListener('click', () => {
-        window.location.href = 'https://www.youtube.com';
+function handleSymbol(symbol) {
+  switch (symbol) {
+    case "C":
+      buffer = "0";
+      runningTotal = 0;
+      break;
+    case "=":
+      if (previousOperator === null) {
+        return;
+      }
+      flushOperation(parseInt(buffer));
+      previousOperator = null;
+      buffer = "" + runningTotal;
+      runningTotal = 0;
+      break;
+    case "←":
+      if (buffer.length === 1) {
+        buffer = "0";
+      } else {
+        buffer = buffer.substring(0, buffer.length - 1);
+      }
+      break;
+    case "+":
+    case "−":
+    case "×":
+    case "÷":
+      handleMath(symbol);
+      break;
+  }
+}
+
+function handleMath(symbol) {
+  if (buffer === "0") {
+    return;
+  }
+
+  const intBuffer = parseInt(buffer);
+
+  if (runningTotal === 0) {
+    runningTotal = intBuffer;
+  } else {
+    flushOperation(intBuffer);
+  }
+
+  previousOperator = symbol;
+
+  buffer = "0";
+}
+
+function flushOperation(intBuffer) {
+  if (previousOperator === "+") {
+    runningTotal += intBuffer;
+  } else if (previousOperator === "−") {
+    runningTotal -= intBuffer;
+  } else if (previousOperator === "×") {
+    runningTotal *= intBuffer;
+  } else if (previousOperator === "÷") {
+    runningTotal /= intBuffer;
+  }
+}
+
+function init() {
+  document
+    .querySelector(".buttons")
+    .addEventListener("click", function (event) {
+      buttonClick(event.target.innerText);
     });
-} else {
-    console.warn('Button with id "button2" not found.');
 }
+
+init();
