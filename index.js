7dec24f652aece81d88dd93147ff9c709407b6f3
Filip Hultén <filip.hulten@si.se>
2026-04-11 02:01:14 +0200

    Update 2

--- index.js
+++ index.js
@@ -7,3 +7,13 @@ if (btn) {
 } else {
 	console.warn('Button with id "button" not found.');
 }
+
+const btn2 = document.getElementById('button2');
+
+if (btn2) {
+    btn2.addEventListener('click', () => {
+        window.location.href = 'https://www.youtube.com';
+    });
+} else {
+    console.warn('Button with id "button2" not found.');
+}
